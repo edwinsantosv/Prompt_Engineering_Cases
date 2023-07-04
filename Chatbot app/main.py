f9b513b15c1c1cbef0f807e5f18e48ccd8604383
Edwin Rafael Vidal Santos <edwin.santos@pucp.pe>
2023-07-04 00:06:20 -0500

    chatbot project developed

---
+++ Chatbot app/main.py
@@ -0,0 +1,194 @@
+# https://medium.com/@avra42/how-to-build-a-chatbot-with-chatgpt-api-and-a-conversational-memory-in-python-8d856cda4542
+
+# Imports
+import os 
+from langchain.llms import OpenAI
+from langchain.document_loaders import TextLoader
+from langchain.document_loaders import PyPDFLoader
+from langchain.indexes import VectorstoreIndexCreator
+import streamlit as st
+from streamlit_chat import message
+import json
+import openai
+from langchain.chains import ConversationChain
+from langchain.chains.conversation.memory import ConversationEntityMemory
+from langchain.chains.conversation.prompt import ENTITY_MEMORY_CONVERSATION_TEMPLATE
+from langchain.llms import OpenAI
+from langchain.prompts.prompt import PromptTemplate
+
+# Set API keys and the models to use
+# Open the JSON file
+with open('/Users/edwin/Repositories/api_keys.json') as file:
+    data = json.load(file)
+
+# Access the API keys
+openai.api_key= data['apiKeys']['OpenAI']
+
+os.environ["OPENAI_API_KEY"] = data['apiKeys']['OpenAI']
+
+model_id = "gpt-3.5-turbo"
+
+
+
+# Set up the Streamlit app layout
+st.title("🏠 Valia Bot 🤖")
+st.markdown(
+        ''' 
+        > :black[**A Chatbot that remembers,**  *powered by -  [LangChain]('https://langchain.readthedocs.io/en/latest/modules/memory.html#memory') + 
+        [OpenAI]('https://platform.openai.com/docs/models/gpt-3-5') + 
+        [Streamlit]('https://streamlit.io') + [DataButton](https://www.databutton.io/)*]
+        ''')
+
+if "generated" not in st.session_state:
+    st.session_state["generated"] = []
+if "past" not in st.session_state:
+    st.session_state["past"] = []
+if "input" not in st.session_state:
+    st.session_state["input"] = ""
+if "stored_session" not in st.session_state:
+    st.session_state["stored_session"] = []
+
+def get_text():
+    """
+    Get the user input text.
+    Returns:
+        (str): The text entered by the user
+    """
+    input_text = st.text_input("You: ", st.session_state["input"], key="input",
+                            placeholder="Hi! I'm Valia's virtual assistant. How can I help you? ...", 
+                            label_visibility='hidden')
+    return input_text
+
+def new_chat():
+    """
+    Clears session state and starts a new chat.
+    """
+    save = []
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        save.append("User:" + st.session_state["past"][i])
+        save.append("Bot:" + st.session_state["generated"][i])        
+    st.session_state["stored_session"].append(save)
+    st.session_state["generated"] = []
+    st.session_state["past"] = []
+    st.session_state["input"] = ""
+    st.session_state.entity_memory.store = {}
+    st.session_state.entity_memory.buffer.clear()
+
+
+with st.sidebar.expander(" 🛠️ Settings ", expanded=False):
+    # Option to preview memory store
+    if st.checkbox("Preview memory store"):
+        st.write(st.session_state.entity_memory.store)
+    # Option to preview memory buffer
+    if st.checkbox("Preview memory buffer"):
+        st.write(st.session_state.entity_memory.buffer)
+    MODEL = st.selectbox(label='Model', options=['gpt-3.5-turbo','text-davinci-003','text-davinci-002','code-davinci-002'])
+    K = st.number_input(' (#)Summary of prompts to consider',min_value=3,max_value=1000)
+
+
+# Set up the Streamlit app layout
+# st.markdown(" > Powered by -  🦜 LangChain + OpenAI + Streamlit")
+
+# Ask the user to enter their OpenAI API key
+API_O = st.sidebar.text_input(":blue[Enter Your OPENAI API-KEY :]", 
+                placeholder="Paste your OpenAI API key here (sk-...)",
+                type="password") # Session state storage would be ideal
+
+
+if API_O:
+    # Create an OpenAI instance
+    llm = OpenAI(temperature=0,
+                openai_api_key=API_O, 
+                model_name=MODEL, 
+                verbose=False) 
+
+
+    # Create a ConversationEntityMemory object if not already created
+    if 'entity_memory' not in st.session_state:
+            st.session_state.entity_memory = ConversationEntityMemory(llm=llm, k=K )
+    
+    template ="""
+    You are Valia's chatbot to help appraise properties. 
+    You always greet users saying "Hi! I'm Valia's virtual assistant. How can I help you?"
+    Then, your goal is to collect the following information:
+    1. Exact address. E.g. "Street 123, District, City, Country"
+    2. Listing type. The options are House, Apartment, Office, Land plot, Commercial property, or Industrial property.
+    3. Operation type. The options are Sell or Rent.
+    4. Total area. The values need to be in squared meters.
+    5. Number of bedrooms.
+    6. Property age. 
+
+    Ask for one item at a time. 
+    Speak in english.
+    Be friendly
+    Be conversational.
+    Be concise. 
+
+    After all the items are collected, make a list of bullets with the items, and ask if the information is correct.
+
+    After the information is confirmed as correct, don't ask "if there is anything else to help with", just say "Great! I will process the data, please wait a moment." as the final message.
+
+    Never return a price estimate even if asked to do so. Just collect the required information. 
+
+    Current conversation:
+    {history}
+    Human: {input}
+    AI Assistant:
+    """
+
+    PROMPT = PromptTemplate(input_variables=["history", "input"],template=template)
+
+
+        # Create the ConversationChain object with the specified configuration
+    Conversation = ConversationChain(
+            llm=llm, 
+            prompt=ENTITY_MEMORY_CONVERSATION_TEMPLATE,
+            memory=st.session_state.entity_memory
+        )  
+else:
+    st.markdown(''' 
+        ```
+        - 1. Enter API Key + Hit enter 🔐 
+
+        - 2. Ask anything via the text input widget
+
+        Your API-key is not stored in any form by this app. However, for transparency ensure to delete your API once used.
+        ```
+        
+        ''')
+    st.sidebar.warning('API key required to try this app.The API key is not stored in any form.')
+    # st.sidebar.info("Your API-key is not stored in any form by this app. However, for transparency ensure to delete your API once used.")
+
+st.sidebar.button("New Chat", on_click = new_chat, type='primary')
+
+user_input = get_text()
+if user_input:
+    output = Conversation.run(input=user_input)  
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(output)
+
+# Allow to download as well
+download_str = []
+# Display the conversation history using an expander, and allow the user to download it
+with st.expander("Conversation", expanded=True):
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        st.info(st.session_state["past"][i],icon="🧐")
+        st.success(st.session_state["generated"][i], icon="🤖")
+        download_str.append(st.session_state["past"][i])
+        download_str.append(st.session_state["generated"][i])
+    
+    # Can throw error - requires fix
+    download_str = '\n'.join(download_str)
+    if download_str:
+        st.download_button('Download',download_str)
+
+
+# Display stored conversation sessions in the sidebar
+for i, sublist in enumerate(st.session_state.stored_session):
+        with st.sidebar.expander(label= f"Conversation-Session:{i}"):
+            st.write(sublist)
+
+# Allow the user to clear all stored conversation sessions
+if st.session_state.stored_session:   
+    if st.sidebar.checkbox("Clear-all"):
+        del st.session_state.stored_session
